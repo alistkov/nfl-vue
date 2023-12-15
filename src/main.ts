f9c7e2d85f1aa14670f08086c64a15d0eb50207c
Anton Listkov <antonlistkov@gmail.com>
2023-12-16 00:11:08 +0100

    style: add tailwind styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from '@/router'
 
+import '@/assets/index.css'
+
 import App from './App.vue'
 
 const app = createApp(App)
