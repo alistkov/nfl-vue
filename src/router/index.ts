54112220a64648c94fd2e0a25b3fa1e02ed01bf8
Anton Listkov <antonlistkov@gmail.com>
2023-12-16 01:24:24 +0100

    feat: add teams and games routes and views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-const StandingsViewVue = () => import('@/views/StandingsView.vue')
+const StandingsView = () => import('@/views/StandingsView.vue')
+const TeamsView = () => import('@/views/TeamsView.vue')
+const GamesView = () => import('@/views/GamesView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      redirect: { name: 'standings' }
+    },
+    {
+      path: '/standings',
       name: 'standings',
-      component: StandingsViewVue
-    }
+      component: StandingsView
+    },
+    {
+      path: '/teams',
+      name: 'teams',
+      component: TeamsView
+    },
+    {
+      path: '/games',
+      name: 'games',
+      component: GamesView
+    },
   ]
 })
 
