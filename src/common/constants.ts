0090f1eb6405c4dd42f61194973128647d798f46
Anton Listkov <antonlistkov@gmail.com>
2023-12-27 21:44:29 +0100

    feat: add main navigation links constant

---
+++ src/common/constants.ts
@@ -0,0 +1,7 @@
+import type { Link } from '@/common/types';
+
+export const MAIN_NAVIGATION: Link[] = [
+  { label: 'Standings', routeName: 'standings' },
+  { label: 'Games', routeName: 'games' },
+  { label: 'Teams', routeName: 'teams' },
+];
