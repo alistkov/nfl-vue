853cea484097aa11793ee3da3dbbad78cc341a46
Anton Listkov <antonlistkov@gmail.com>
2023-12-29 02:25:41 +0100

    style: format division table component

--- src/modules/standings/components/DivisionTable.vue
+++ src/modules/standings/components/DivisionTable.vue
@@ -1,53 +1,53 @@
-<script setup lang='ts'>
-import { ApiService } from '@/services/api';
-import type { Team } from '@/common/types';
-import { ref } from 'vue';
+<script setup lang="ts">
+import { ApiService } from '@/services/api'
+import type { Team } from '@/common/types'
+import { ref } from 'vue'
 
 const props = defineProps({
   division: {
     type: String,
-    required: true,
+    required: true
   }
-});
+})
 
-const standings = ref<Team[]>();
-const error = ref<string | null>(null);
-const loaded = ref<boolean>(false);
-const loading = ref<boolean>(false);
-const apiService = new ApiService();
+const standings = ref<Team[]>()
+const error = ref<string | null>(null)
+const loaded = ref<boolean>(false)
+const loading = ref<boolean>(false)
+const apiService = new ApiService()
 
 const calculatePtc = (won: number, lost: number, ties: number): number => {
-  const totalGames = won + lost + ties;
+  const totalGames = won + lost + ties
   if (ties === 0) {
-    return won / totalGames;
+    return won / totalGames
   }
-  return (((won + ties) / totalGames + (won + ties) / totalGames) / 2);
-};
+  return ((won + ties) / totalGames + (won + ties) / totalGames) / 2
+}
 
 const calculatePtcFromString = (result: string): number => {
-  const [won = 0, lost = 0, ties = 0] = result.split('-');
+  const [won = 0, lost = 0, ties = 0] = result.split('-')
   return calculatePtc(Number(won), Number(lost), Number(ties))
-};
+}
 
 const getDivisionStandings = async (): Promise<void> => {
-  loading.value = true;
+  loading.value = true
   try {
-    standings.value = await apiService.getDivisionStandings(1, 2023, props.division);
+    standings.value = await apiService.getDivisionStandings(1, 2023, props.division)
   } catch (err) {
     if (err instanceof Error) {
-      error.value = err.message;
+      error.value = err.message
     }
   }
-  loaded.value = true;
-  loading.value = false;
-};
+  loaded.value = true
+  loading.value = false
+}
 </script>
 
 <template>
   <table class="w-full bg-white mb-4">
     <tr class="font-normal text-xs">
-      <th class="text-left font-normal border-b border-gray-mid px-[20px] py-[15px] border-r-2 w-[300px]">{{
-        props.division.toUpperCase() }}
+      <th class="text-left font-normal border-b border-gray-mid px-[20px] py-[15px] border-r-2 w-[300px]">
+        {{ props.division.toUpperCase() }}
       </th>
       <template v-if="loaded">
         <th class="font-normal border-b border-gray-mid px-[20px] py-[15px]">W</th>
@@ -73,9 +73,7 @@ const getDivisionStandings = async (): Promise<void> => {
     </tr>
     <template v-if="loading">
       <tr>
-        <td class="px-[20px] py-[15px] text-xs text-center" colspan="15">
-          Loading...
-        </td>
+        <td class="px-[20px] py-[15px] text-xs text-center" colspan="15">Loading...</td>
       </tr>
     </template>
     <template v-if="error">
@@ -90,36 +88,52 @@ const getDivisionStandings = async (): Promise<void> => {
         :class="{ 'bg-gray-light': (index + 1) % 2 === 0 }">
         <td class="font-normal border-b border-gray-mid px-[20px] py-[10px] border-r-2">
           <a href="#" class="flex items-center">
-            <img :src="team.team.logo" :alt="team.team.name" class="w-[24px] mr-2">
+            <img :src="team.team.logo" :alt="team.team.name" class="w-[24px] mr-2" />
             {{ team.team.name }}
           </a>
         </td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ team.won }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ team.lost }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ team.ties }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ calculatePtc(team.won,
-          team.lost,
-          team.ties).toFixed(3) }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ team.points.for }}
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.won }}
         </td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ team.points.against }}
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.lost }}
         </td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px] border-r">{{
-          team.points.difference
-        }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ team.records.home }}
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.ties }}
         </td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px] border-r">{{ team.records.road }}
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ calculatePtc(team.won, team.lost, team.ties).toFixed(3) }}
         </td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ team.records.division
-        }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px] border-r">{{
-          calculatePtcFromString(team.records.division).toFixed(3) }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{
-          team.records.conference }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px] border-r">{{
-          calculatePtcFromString(team.records.conference).toFixed(3) }}</td>
-        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">{{ team.streak }}</td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.points.for }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.points.against }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px] border-r">
+          {{ team.points.difference }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.records.home }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px] border-r">
+          {{ team.records.road }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.records.division }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px] border-r">
+          {{ calculatePtcFromString(team.records.division).toFixed(3) }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.records.conference }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px] border-r">
+          {{ calculatePtcFromString(team.records.conference).toFixed(3) }}
+        </td>
+        <td class="text-center font-normal border-b border-gray-mid px-[20px] py-[10px]">
+          {{ team.streak }}
+        </td>
       </tr>
     </template>
   </table>
