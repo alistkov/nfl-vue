34070225354e3d969bcb16595bae9767699bbb11
Anton Listkov <antonlistkov@gmail.com>
2024-01-05 23:01:27 +0100

    feat: add conferences table

--- src/modules/standings/components/ConferencesStandings.vue
+++ src/modules/standings/components/ConferencesStandings.vue
@@ -1,9 +1,12 @@
 <script setup lang='ts'>
-
+import StandingsTable from '@/modules/standings/components/StandingsTable.vue';
+import { CONFERENCES } from '@/common/constants';
 </script>
 
 <template>
-  Conferences
+  <div v-for="conference of CONFERENCES" :key="conference.title" class="mb-10">
+    <StandingsTable standings-type="conference" :standings-title="conference.title" />
+  </div>
 </template>
 
 <style scoped></style>
