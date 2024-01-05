07ec8d6bcf8232b21e831199ea336676c045f33d
Anton Listkov <antonlistkov@gmail.com>
2024-01-05 22:41:43 +0100

    refactor: refactor code

--- src/modules/standings/StandingsComponent.vue
+++ src/modules/standings/StandingsComponent.vue
@@ -8,8 +8,9 @@ import { STANDINGS_NAVIGATION } from '@/common/constants';
       <li v-for="link of STANDINGS_NAVIGATION" :key="link.label" class="w-[50%] md:w-[270px]">
         <Router-Link :to="{ name: link.routeName }"
           class="border border-solid border-blue-light w-full block text-center text-xs uppercase p-2 md:p-2.5 tracking-wide hover:text-white hover:bg-blue-light transition duration-300"
-          active-class="bg-blue-light text-white">{{ link.label
-          }}</Router-Link>
+          active-class="bg-blue-light text-white">
+          {{ link.label }}
+        </Router-Link>
       </li>
     </ul>
   </nav>
