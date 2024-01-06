de6c126485296ffac46c4d6ec7c5a862e6b4445e
Anton Listkov <antonlistkov@gmail.com>
2024-01-06 02:31:05 +0100

    refactor: refactor teams view

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,47 +1,9 @@
 <script setup lang='ts'>
-import { onMounted, ref } from 'vue';
-import { ApiService } from '@/services/api';
-import type { TeamInfo } from '@/common/types';
-import LoaderComponent from '@/common/components/LoaderComponent.vue'
-
-const apiService = new ApiService();
-const teams = ref<TeamInfo[]>();
-const error = ref<string | null>(null);
-const loading = ref<boolean>(false);
-
-const fetchTeams = async (): Promise<void> => {
-  loading.value = true;
-  try {
-    const response = await apiService.getTeams({ league: 1, season: 2023 });
-    teams.value = response.filter((team: TeamInfo) => team.code !== null);
-  } catch (err) {
-    if (err instanceof Error) {
-      error.value = err.message
-    }
-  }
-  loading.value = false;
-}
-
-onMounted(async () => {
-  fetchTeams();
-})
+import TeamComponent from '@/modules/teams/TeamsComponent.vue';
 </script>
 
 <template>
-  <div v-if="loading">
-    <LoaderComponent />
-  </div>
-  <div v-if="error">
-    {{ error }}
-  </div>
-  <div class="flex flex-wrap mx-[-10px]">
-    <div v-for="team in teams" :key="team.id" class="w-full lg:w-[25%] md:w-[50%] mb-2 px-[10px]">
-      <a href="#"
-        class="block bg-gray-light border border-gray-dark p-2 text-sm hover:text-white hover:bg-blue-light transition-colors duration-300">
-        {{ team.name }}
-      </a>
-    </div>
-  </div>
+  <TeamComponent />
 </template>
 
 <style scoped></style>
