e0d4f1023f8b17672ac3f169f8456ebd8cd6077b
Anton Listkov <antonlistkov@gmail.com>
2024-01-20 22:03:45 +0100

    refactor: remove games link

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,7 +2,6 @@ import type { Link } from '@/common/types';
 
 export const MAIN_NAVIGATION: Link[] = [
   { label: 'Standings', routeName: 'standings' },
-  { label: 'Games', routeName: 'games' },
   { label: 'Teams', routeName: 'teams' },
 ];
 
