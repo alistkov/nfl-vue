75e5c868887162ef3349334975eb06e35c841875
Anton Listkov <antonlistkov@gmail.com>
2024-01-20 22:01:51 +0100

    feat: add link on NFL table

--- src/modules/standings/components/ConferencesStandings.vue
+++ src/modules/standings/components/ConferencesStandings.vue
@@ -8,6 +8,10 @@ import ErrorAlert from '@/common/components/ErrorAlert.vue';
   <ErrorAlert class="mb-5">
     <p class="font-bold">Warning!</p>
     <p>Tables do not take into account additional indicators in case of equal victories.</p>
+    <p>The current standings can be found on the <a href="https://www.nfl.com/standings/conference/2023/REG"
+        target="_blank"
+        class="font-bold hover:border-b hover:border-red-600 transition-colors ease-in duration-200">official NFL
+        website</a>.</p>
   </ErrorAlert>
   <div v-for="conference of CONFERENCES" :key="conference.title" class="mb-10">
     <StandingsTable standings-type="conference" :standings-title="conference.title" />
